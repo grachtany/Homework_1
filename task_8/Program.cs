1e58c7189b8551f0ded24f0c4ad9333bc428b1cf
tatangracheva <grachtany@yandex.ru>
2022-11-22 20:33:39 +0300

    Выполнена задача 8. показать четные до Н

---
+++ task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.Write("Введите число a - ");
+int numberA = int.Parse(Console.ReadLine());
+int count=1;
+while (count<=numberA)
+        { int rezult = count%2;
+           if (rezult==0) Console.Write($" {count} , ");
+           else Console.Write($" ");
+            
+            count++;
+        }
