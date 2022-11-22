c4914627d0da97049d1dce9cfe2ddc2cf30be29d
tatangracheva <grachtany@yandex.ru>
2022-11-22 20:10:30 +0300

    Выполнена задача 6. Определить честность числа

---
+++ task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Введите число a - ");
+int numberA = int.Parse(Console.ReadLine());
+int rezult = numberA%2;
+if (rezult==0) Console.WriteLine($"Число {numberA} - четное" );
+    else
+    Console.WriteLine($"Число {numberA} - нечетное" );
